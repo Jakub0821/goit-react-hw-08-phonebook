8fcefbc91a03ffd9fb2dfd96ee2f0bd838eb870d
Jakub0821 <jakubmigda120@gmail.com>
2024-03-07 21:37:11 +0100

    change App.jsx, package.json and package-lock.json

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,71 @@
+import { useEffect, lazy } from "react";
+import { useDispatch } from "react-redux";
+import { fetchContacts } from "redux/contacts/operations";
+import { Route, Routes } from "react-router-dom";
+import { RestrictedRoute } from "./RestrictedRoute/RestrictedRoute";
+import { PrivateRoute } from "./PrivateRoute/PrivateRoute";
+import { refreshUser } from "redux/auth/operations";
+import { useAuth } from "hooks/useAuth";
+import { Layout } from "../layouts/RootLayout/RootLayout";
+
+const LoginPage = lazy(() =>
+  import("pages/Login/Login" /* webpackChunkName: "login-page" */)
+);
+const RegisterPage = lazy(() =>
+  import("pages/Register/Register" /* webpackChunkName: "register-page" */)
+);
+const PhonebookPage = lazy(() =>
+  import("pages/Phonebook/Phonebook" /* webpackChunkName: "phonebook-page" */)
+);
+const HomePage = lazy(() =>
+  import("pages/Home/Home" /* webpackChunkName: "home-page" */)
+);
+
 export const App = () => {
+  const dispatch = useDispatch();
+  const { isLoggedIn } = useAuth();
+
+  useEffect(() => {
+    dispatch(refreshUser());
+    if (isLoggedIn) dispatch(fetchContacts());
+  }, [dispatch, isLoggedIn]);
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div className="flex flex-col items-center pt-4">
+      <div className="w-4/6">
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<HomePage />} />
+            <Route
+              path="/register"
+              element={
+                <RestrictedRoute
+                  redirectTo="/contacts"
+                  component={<RegisterPage />}
+                />
+              }
+            />
+            <Route
+              path="/login"
+              element={
+                <RestrictedRoute
+                  redirectTo="/contacts"
+                  component={<LoginPage />}
+                />
+              }
+            />
+            <Route
+              path="/contacts"
+              element={
+                <PrivateRoute
+                  redirectTo="/login"
+                  component={<PhonebookPage />}
+                />
+              }
+            />
+          </Route>
+        </Routes>
+      </div>
     </div>
   );
 };
